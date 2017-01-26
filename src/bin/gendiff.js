09869916cda3fcaa64b435782da487f238213f83
Vitaly Usov <v.usov@ctm.ru>
2017-01-26 11:46:48 +0300

    bin changed

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,13 +2,22 @@
 
 // @flow
 import program from 'commander';
+import fs from 'fs';
 import parse from '../jsonparse';
-import diff from '../diff';
+import { diff, toString } from '../diff';
 
 program
   .version('0.3.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<first_config> <second_config>')
-  .action((first, second) => console.log(first))
+  .action((first, second) => {
+    const beforeFile = fs.readFileSync(first, 'utf-8');
+    const afterFile = fs.readFileSync(second, 'utf-8');
+
+    const beforeObj = parse(beforeFile);
+    const afterObj = parse(afterFile);
+    const d = diff(beforeObj, afterObj);
+    console.log(toString(d));
+  })
   .parse(process.argv);
