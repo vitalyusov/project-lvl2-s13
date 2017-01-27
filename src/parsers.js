98a44aeb02e339ee97c45539e3e12bf0e9769e4a
Vitaly Usov <v.usov@ctm.ru>
2017-01-27 18:17:31 +0300

    parsers changes

--- src/parsers.js
+++ src/parsers.js
@@ -2,15 +2,4 @@ import jsonparse from './jsonparse';
 import yamlparse from './yamlparse';
 import iniparse from './iniparse';
 
-export default (ext) => {
-  switch (ext) {
-    case '.json':
-      return jsonparse;
-    case '.yml':
-      return yamlparse;
-    case '.ini':
-      return iniparse;
-    default:
-      throw "No parser found for provided extension";
-  }
-};
+export default ext => ({ '.json': jsonparse, '.yml': yamlparse, '.ini': iniparse })[ext];
